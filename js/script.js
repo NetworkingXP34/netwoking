cb5a61d1702ade023ca672a08cd3db1acd2db977
Luiz Santos <luizcsbh@gmail.com>
2020-07-06 15:39:03 -0300

    Alteração no home

--- js/script.js
+++ js/script.js
@@ -13,10 +13,10 @@ function atualizaContador() {
     var dd = parseInt(hh / 24); 
     ss = ss - (mm * 60); mm = mm - (hh * 60); hh = hh - (dd * 24); 
     var faltam = ''; 
-    faltam += (dd && dd > 1) ? dd+' dias, ' : (dd==1 ? '1 dia, ' : ''); 
-    faltam += (toString(hh).length) ? hh+' horas, ' : ''; 
-    faltam += (toString(mm).length) ? mm+' min e ' : ''; 
-    faltam += ss+' seg'; if (dd+hh+mm+ss > 0) { 
+    faltam += (dd && dd > 1) ? dd+' dia(s), ' : (dd==1 ? '1 dia, ' : ''); 
+    faltam += (toString(hh).length) ? hh+' hora(s), ' : ''; 
+    faltam += (toString(mm).length) ? mm+' minuto(s) e ' : ''; 
+    faltam += ss+' seg.'; if (dd+hh+mm+ss > 0) { 
         document.getElementById('contador').innerHTML = faltam; 
         setTimeout(atualizaContador,1000); 
     } else { 
